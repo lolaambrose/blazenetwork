8a8930b53fe0e8a39d7648030ec93bc30b0ae543
♡ ihateheroin *✧⁺˚ <163886741+lolaambrose@users.noreply.github.com>
2024-05-27 18:06:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 # telegram
 TELEGRAM_TOKEN = os.getenv(
-    "TELEGRAM_TOKEN", "5656984388:AAFevfzKm_qkPBAWFBmRXLl6600jJgm8n8A"
+    "TELEGRAM_TOKEN", "5656984388:"
 )
 TELEGRAM_ADMINS = [6113190687, 6405543408]
 TELEGRAM_CHANNEL = "@blazesecurity"
@@ -12,7 +12,7 @@ MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
 
 # merchant
 MERCHANT_API_URL = "https://api.cryptomus.com/v1"
-MERCHANT_UUID = "57f83d2b-0d03-4053-8d24-fd3d29e42f96"
-MERCHANT_API_KEY = "5y9HhRUgIQcL1JYmtt7epbos8bflQkMSBrqjCpD7RLrLI2ZkqkxLl4ke6EUJo5wvYaTuvLeM9RaUBCTjIACTxDFJxvhdwuEdutRYEnoCpWH8zPVo7gTBEVsriweQr7bK"
-MERCHANT_CALLBACK = "https://api.blazenet.work/v1/merchant/callback"
-MERCHANT_CALLBACK_ORIGIN = "91.227.144.54"
+MERCHANT_UUID = "57f83d2b"
+MERCHANT_API_KEY = "oCpWH8zPVo7gTBEVsriweQr7bK"
+MERCHANT_CALLBACK = "https://"
+MERCHANT_CALLBACK_ORIGIN = ""
